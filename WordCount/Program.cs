64012827623aff9a00e30aaeabe000c2cb95dd9d
nielshenriksen <niels@niels-henriksen.dk>
2023-08-25 14:34:43 +0200

    Added Lib to make unittest more easy

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -1,27 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿
 var file1 = "Go do that thing that you do so well";
 var file2 = "I play football well";
 
-var f = file1 + " " + file2;
+var core = new WordCountLib.Core();
 
-var found = f.Split(" ");
+core.AddText(file1);
+core.AddText(file2);
 
-var words = new Dictionary<string, int>();
-
-foreach (var word in found) { 
-    if(words.ContainsKey(word)) 
-    { 
-        words[word]++;
-    }
-    else {
-        words.Add(word, 1);
-    }
-}    
-
-foreach(var word in words) {
-    Console.WriteLine(word.Key  + " " + word.Value);
-}
-
+Console.WriteLine(core.ToString());
 Console.ReadLine();
---
+++ WordCountLib/Core.cs
@@ -0,0 +1,74 @@
+﻿namespace WordCountLib
+{
+    public class Core
+    {
+        private Dictionary<string, int> words = new Dictionary<string, int>();
+
+        public Core()
+        {
+            ForceCaseSensitive = false;
+        }
+
+        public Core(bool forceCaseSensitive)
+        {
+            ForceCaseSensitive = forceCaseSensitive;
+        }
+
+        /// <summary>
+        /// Force the text to be case sensitive or not
+        /// </summary>
+        public bool ForceCaseSensitive { get; set; }
+
+        /// <summary>
+        /// Add a text string to the wordlist.
+        /// </summary>
+        /// <param name="text"></param>
+        public void AddText(string text)
+        {
+            var found = text.Split(" ");
+
+            foreach (var word in found) {
+                
+                // we dont count space
+                if(String.IsNullOrEmpty(word)) continue;
+
+                // regex for letters and numbers
+
+
+                if (words.ContainsKey(word)) {
+                    words[word]++;
+                }
+                else {
+                    words.Add(word, 1);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// returns the count for how many times a word is found
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public int Count(string word)
+        {
+            if(!words.ContainsKey(word)) { return 0; }
+
+            return words[word];
+        }
+
+        /// <summary>
+        /// Output the entire list of words and how many time its in the list
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            var response = "";
+            foreach (var word in words) {
+                response += $"{word.Key} {word.Value}";
+            }
+
+            return response;
+        }
+    }
+}
